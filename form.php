8fd0545e9a56c210d846e71c6c23d87a5b47f0f6
Felipe M. Frudeli <51006430+FelipeMartinsFrudeli@users.noreply.github.com>
2020-01-18 23:19:48 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Form</title>
+ <meta charset="utf-8">
+ </head>
+ <body>
+ 
+ <div>
+<?php
+$valor = $_GET['v'];
+echo "O valor enviado foi: $valor";
+?>
+
+<br/>
+<br/>
+<a href="index.html">Voltar</a>
+
+</div>
+ 
+
+</body>
+</html>
